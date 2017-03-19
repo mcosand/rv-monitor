80bb64e5ee77555a4a2c08d2af0eb903720b555f
Matt Cosand <matt@cosand.net>
2017-03-18 23:23:00 -0700

    change event name

--- web/server.js
+++ web/server.js
@@ -22,13 +22,14 @@ server.route({
 
 function storeData(d, reply) {
   var row = {
-    type: d.name,
+    type: d.event,
     data: d.data,
     raw: JSON.stringify(d),
     published: new Date(d.published)
   }
+  console.log(row)
   return db.insert(row).into("packets").then(function (id) {
-    console.log(id[0], d.name, d.data);
+    console.log(id[0], d.event, d.data);
   }).then(() => reply({}).code(200));
 }
 
