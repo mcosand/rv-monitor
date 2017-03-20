151a396f6ad81b8f66db2dc557635ad2c9eedfcc
Matt Cosand <matt@cosand.net>
2017-03-19 21:33:47 -0700

    web site updates

--- web/server.js
+++ web/server.js
@@ -20,43 +20,34 @@ server.route({
   }
 });
 
-function storeData(d, reply) {
-  var row = {
-    type: d.event,
-    data: d.data,
-    raw: JSON.stringify(d),
-    published: new Date(d.published)
+server.route({
+  method: 'GET',
+  path: '/last',
+  handler: function(request, reply) {
+    return reply(db('packets').whereNotNull('published').limit(1))
   }
-  console.log(row)
-  return db.insert(row).into("packets").then(function (id) {
-    console.log(id[0], d.event, d.data);
-  })
-  .then(() => reply({}).code(200))
-  .catch((err) => {
-    console.log(err)
-    return reply(err + '').code(500)
-  })
-}
+})
 
 server.route({
   method: 'POST',
   path:'/particle',
   handler: function(request, reply) {
-    return storeData(request.payload, reply)
-  }
-})
-server.route({
-  method: 'PUT',
-  path: '/particle',
-  handler: function(request, reply) {
-    return storeData(request.payload, reply);
-  }
-})
-server.route({
-  method: 'GET',
-  path:'/particle',
-  handler: function(request, reply) {
-    return storeData(request.url.query, reply)
+    const d = request.payload
+    var row = {
+      type: d.event,
+      data: d.data,
+      raw: JSON.stringify(d),
+      published: new Date(d.published_at)
+    }
+    console.log(row.published)
+    return db.insert(row).into("packets").then(function (id) {
+      console.log(id[0], d.event, d.data);
+    })
+    .then(() => reply({}).code(200))
+    .catch((err) => {
+      console.log(err)
+      return reply(err + '').code(500)
+    })
   }
 })
 
